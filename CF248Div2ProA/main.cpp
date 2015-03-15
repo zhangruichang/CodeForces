f76cf41322cfcd40e3b3e31e5dc3e5e585dc43c2
zhangruichang <zhangruichang112@gmail.com>
2015-03-16 07:38:17 +0800

    CF248 249

---
+++ CF248Div2ProA/main.cpp
@@ -0,0 +1,81 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+#define f first
+#define s second
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    if(!m) return n;
+    return GCD(n%m, m);//yushu and chushu
+}
+int a[maxn];
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    int n;
+    while(cin>>n)
+    {
+        int sum=0 ,ones=0, twos=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i],sum+=a[i];
+            if(a[i]==100) ones++;
+            else twos++;
+        }
+        bool ok=0;
+        for(int i=0;i<=twos;i++)
+        {
+            int remain=sum/2-i*200;
+            if(remain<0) break;
+            if(remain%100==0 && remain/100<=ones) {ok=1;break;}
+        }
+        if(ok) puts("YES");
+        else puts("NO");
+     }
+	return 0;
+}
---
+++ CF249Div2ProB/main.cpp
@@ -0,0 +1,77 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+#define f first
+#define s second
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    if(!m) return n;
+    return GCD(n%m, m);//yushu and chushu
+}
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    string s;int k;
+    while(cin>>s>>k)
+    {
+        int n=s.size(), remain=k;
+        for(int i=0;i<n && remain>0;i++)
+        {
+            int len;
+            for(len=remain+1;len>=2;len--)
+            {
+                if(i+len-1>=n) continue;
+                if(s[i]<s[i+len-1]) break;
+            }
+            remain-=(len-1);
+            for(int j=i+len-2;j>=i;j--) swap(s[j], s[j+1]);
+        }
+        cout<<s<<endl;
+    }
+	return 0;
+}

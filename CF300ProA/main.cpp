99b51396f080260ba1e0b5f09c2da65522181c02
zhangruichang <zhangruichang112@gmail.com>
2015-05-07 17:16:50 +0800

    CF 300

---
+++ CF300ProA/main.cpp
@@ -0,0 +1,111 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int a[maxn], n, t, m;
+
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    string s;
+    while(cin>>s)
+    {
+        int i, sn=s.size();
+        string t="CODEFORCES";int tn=t.size();
+        for(i=0;i<tn && i<sn;i++)
+        {
+            if(s[i]!=t[i]) break;
+        }
+        //last t.size()-i
+        if(sn-(tn-i)>=0 && sn-(tn-i)<sn && i<tn && i>=0 && s.substr(sn-(tn-i)) == t.substr(i)) {puts("YES");continue;}
+        for(i=tn-1;i<sn && i>=0;i--)
+        {
+            if(s[i]!=t[i]) break;
+        }
+        if(i+1<=tn && i+1<=sn && s.substr(0, i+1)==t.substr(0, i+1)) {puts("YES");continue;}
+        puts("NO");
+    }
+	return 0;
+}

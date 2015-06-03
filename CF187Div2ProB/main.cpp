2a9e734399f26853a7712e5a12da3e39eecc0857
zhangruichang <zhangruichang112@gmail.com>
2015-06-03 17:14:04 +0800

    CF

---
+++ CF187Div2ProB/main.cpp
@@ -0,0 +1,101 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+LL a[maxn], n, t, m;
+
+
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    cin>>n>>m;LL v, x, y, z;
+    for(LL i=1;i<=n;i++) cin>>a[i];
+    LL sum=0;
+    for(LL i=0;i<m;i++)
+    {
+        cin>>t;
+        if(t==1)
+        {
+            cin>>v>>x;
+            a[v]=x-sum;
+        }
+        else if(t==2)
+        {
+            cin>>y;
+            sum+=y;
+            //for(int i=1;i<=n;i++) a[i]+=y;
+        }
+        else
+        {
+            cin>>z;
+            cout<<a[z]+sum<<endl;
+        }
+    }
+    //cout<<sum<<endl;
+    //for(auto e: assv) cout<<e<<" ";
+    //cout<<endl;
+	return 0;
+}
